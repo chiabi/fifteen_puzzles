5b721bfe5d64a37f0a5afaf13e4197aacf590d18
chiabi <chiabi88@gmail.com>
2018-05-02 04:48:03 +0900

    1차 스크립트 작업  완료

--- index.js
+++ index.js
@@ -1,9 +1,71 @@
 const COL = 4;
-const offset = [];
+let move = 0;
 
-for (let i = 0, k = 0; i < COL; i++) {
-  for (let j = 0; j < COL; j++, k++) {
-    offset.push({x: j, y: i, o: k});
+const moveEl = document.querySelector('.move');
+const blankEl = document.querySelector('.blank');
+
+function init() {
+  document.querySelectorAll('.box').forEach((item, index) => {
+    item.setAttribute('data-idx', index);
+    moveEl.textContent = (move = 0);
+  });
+}
+
+function moveBox() {
+  const puzzle = this.dataset.idx;
+  const blank = blankEl.dataset.idx;
+  if (Math.abs(blank - puzzle) === 1 || Math.abs(blank - puzzle) === 4) {
+    this.setAttribute('data-idx', blank);
+    blankEl.setAttribute('data-idx', puzzle);
+    moveEl.textContent = ++move;
+  }
+  return false;
+}
+
+function shuffle() {
+  for(let i = 0; i < 50; ) {
+    const blank = parseInt(blankEl.dataset.idx);
+    const random = Math.floor(Math.random() * 4);
+    switch(random) {
+      case 0:
+        if(blank + 1 <= 15) {
+          document.querySelector(`[data-idx="${blank + 1}"]`).setAttribute('data-idx', blank);
+          blankEl.setAttribute('data-idx', blank + 1);
+          i++;
+        }
+        break;
+      case 1:
+        if(blank - 1 >= 0)  {
+          document.querySelector(`[data-idx="${blank - 1}"]`).setAttribute('data-idx', blank);
+          blankEl.setAttribute('data-idx', blank - 1);
+          i++;
+        }
+        break;
+      case 2:
+        if(blank + 4 <= 15) {
+          document.querySelector(`[data-idx="${blank + 4}"]`).setAttribute('data-idx', blank);
+          blankEl.setAttribute('data-idx', blank + 4);
+          i++;
+        }
+        break;
+      case 3:
+        if(blank - 4 >= 0) {
+          document.querySelector(`[data-idx="${blank - 4}"]`).setAttribute('data-idx', blank);
+          blankEl.setAttribute('data-idx', blank - 4);
+          i++;
+        }
+        break;
+    }
   }
 }
-console.log(offset);
+
+// 퍼즐 클릭 이벤트
+document.querySelectorAll('.puzzle').forEach(item => {
+  item.addEventListener('click', moveBox);
+});
+
+// RESET
+document.querySelector('.btn-reset').addEventListener('click', init);
+
+// shuffle
+document.querySelector('.btn-shuffle').addEventListener('click', shuffle);
